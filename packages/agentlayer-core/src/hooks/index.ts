190526e3d2e4702907210d23f1dec044f1262a4a
Kyle Mistele <kyle@humanlayer.dev>
2026-05-01 20:56:22 -0700

    feat: add core output truncation hooks

--- packages/agentlayer-core/src/hooks/index.ts
+++ packages/agentlayer-core/src/hooks/index.ts
@@ -1,5 +1,6 @@
 export * from './approval'
 export * from './deduplicate-reads'
+export * from './output-truncation'
 export * from './post-tool-use'
 export * from './pre-request'
 export * from './pre-tool-use'
